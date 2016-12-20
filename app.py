baa6a6323a657d5d3dce10cd83ff8c66b3f3fb40
Ayhan APAYDIN <ayhanap@gmail.com>
2016-12-20 11:22:16 +0300

    env parameter for flask debug option / ci

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ if __name__ == '__main__':
 
     print "Starting app on port %d" % port
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=os.environ['FLASK_DEBUG'], port=port, host='0.0.0.0')
